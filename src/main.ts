ce533e28f8d70f75695cd47b0a8e83813d1a1ab4
Mikael-R <mikarg9@gmail.com>
2025-10-08 03:19:12 -0300

    feat: update Swagger setup to use SwaggerModule directly and improve console log for Swagger URL

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as swaggerUi from 'swagger-ui-express';
 import { version } from '../package.json';
 import { AppModule } from './app.module';
 
@@ -19,18 +18,18 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
 
-  app.use('/', swaggerUi.serve, swaggerUi.setup(document, {
+  SwaggerModule.setup('docs', app, document, {
     customSiteTitle: 'Clínica Digital API Docs',
     swaggerOptions: {
       persistAuthorization: true,
     },
-  }));
-  const port = process.env.PORT || 3000;
+  });
 
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 
   console.log(`🚀 Aplicação rodando em: http://localhost:${port}`);
-  console.log(`📘 Swagger disponível em: http://localhost:${port}`);
+  console.log(`📘 Swagger disponível em: http://localhost:${port}/docs`);
 }
 
 bootstrap();
