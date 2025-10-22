fa38cba91a9c0007662f2d96ab1556353ec7c77f
Mikael-R <mikarg9@gmail.com>
2025-10-21 21:35:13 -0300

    feat: remove PacienteModule from AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { EstabelecimentoModule } from './estabelecimento/estabelecimento.module';
-import { PacienteModule } from './paciente/paciente.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI ?? 'mongodb://localhost/estabelecimento-digital'),
     EstabelecimentoModule,
-    PacienteModule,
   ],
 })
 export class AppModule { }
