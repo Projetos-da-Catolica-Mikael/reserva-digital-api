1b3b0e5362e608c92e9b8e9072439f5001a07079
Mikael-R <mikarg9@gmail.com>
2025-10-22 12:37:02 -0300

    fix: correct typos in API operation summaries for EstabelecimentoController

--- src/estabelecimento/estabelecimento.controller.ts
+++ src/estabelecimento/estabelecimento.controller.ts
@@ -10,32 +10,32 @@ export class EstabelecimentoController {
   constructor(private readonly estabelecimentoService: EstabelecimentoService) { }
 
   @Get()
-  @ApiOperation({ summary: 'Lista todas os estabelecimentos' })
+  @ApiOperation({ summary: 'Lista todos os estabelecimentos' })
   findAll() {
     return this.estabelecimentoService.findAll();
   }
 
   @Get(':id')
-  @ApiOperation({ summary: 'Busca umo estabelecimento pelo ID' })
+  @ApiOperation({ summary: 'Busca um estabelecimento pelo ID' })
   findOne(@Param('id') id: string) {
     return this.estabelecimentoService.findOne(id);
   }
 
   @Post()
-  @ApiOperation({ summary: 'Cria uma novo estabelecimento' })
+  @ApiOperation({ summary: 'Cria um novo estabelecimento' })
   @ApiResponse({ status: 201, description: 'Estabelecimento criada com sucesso' })
   create(@Body() createEstabelecimentoDto: CreateEstabelecimentoDto) {
     return this.estabelecimentoService.create(createEstabelecimentoDto);
   }
 
   @Put(':id')
-  @ApiOperation({ summary: 'Atualiza umo estabelecimento existente' })
+  @ApiOperation({ summary: 'Atualiza um estabelecimento existente' })
   update(@Param('id') id: string, @Body() updateEstabelecimentoDto: UpdateEstabelecimentoDto) {
     return this.estabelecimentoService.update(id, updateEstabelecimentoDto);
   }
 
   @Delete(':id')
-  @ApiOperation({ summary: 'Remove umo estabelecimento' })
+  @ApiOperation({ summary: 'Remove um estabelecimento' })
   remove(@Param('id') id: string) {
     return this.estabelecimentoService.remove(id);
   }
